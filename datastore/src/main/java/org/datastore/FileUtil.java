5c32c4f72430ad7122659d9e0a4f095bbeffa959
Gaurav Modi <49981853+GauravSModi@users.noreply.github.com>
2025-04-08 13:52:44 -0700

    Added readEncryptedFile method

--- datastore/src/main/java/org/datastore/FileUtil.java
+++ datastore/src/main/java/org/datastore/FileUtil.java
@@ -8,14 +8,13 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class FileUtil {
-
     public static String generateEncryptedFileName(String fileName) {
-        return fileName.replaceAll("[.]", "_") + "otherway" + ".enc";
+        return fileName.replaceAll("[.]", "_") + ".enc";
     }
 
     public static void saveEncryptedFile(String userId, String fileName, byte[] encryptedFileContent) throws IOException {
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        out.write(encryptedFileContent);
+//        ByteArrayOutputStream out = new ByteArrayOutputStream();
+//        out.write(encryptedFileContent);
 
         // TODO: Technically should make sure userId can be a valid folder name
 
@@ -26,5 +25,18 @@ public class FileUtil {
 
         Path path = dir.resolve(generateEncryptedFileName(fileName));
         Files.write(path, encryptedFileContent);
+    }
+
+    public static byte[] readEncryptedFile(String userId, String fileName) throws Exception {
+        byte[] fileContent = null;
+        Path dir = Paths.get("encrypted_files/" + userId);
+        if (!Files.exists(dir) || !Files.isDirectory(dir)) {
+            throw new Exception("Error: user doesn't exist? what?");
+        }
+
+        Path path = dir.resolve(generateEncryptedFileName(fileName));
+
+        fileContent = Files.readAllBytes(path);
+        return fileContent;
     }
 }
